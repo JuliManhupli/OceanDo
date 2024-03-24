0c34f7d48f7c75a960d36103b5c74d148094bb4c
JuliManhupli <juliamangup@gmail.com>
2024-03-24 15:06:24 +0200

    verification and password change done

--- ocean_do/accounts/models.py
+++ ocean_do/accounts/models.py
@@ -14,7 +14,7 @@ class User(AbstractBaseUser):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    # is_verified = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ["username"]
