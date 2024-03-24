e7cec53f8076df5c9f314eb50edacfb35372e736
JuliManhupli <juliamangup@gmail.com>
2024-03-24 15:36:55 +0200

    added sending letters to the mail

--- ocean_do/accounts/views.py
+++ ocean_do/accounts/views.py
@@ -4,7 +4,7 @@ import pyotp
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.hashers import make_password
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib import messages
 
 from .models import User
@@ -30,7 +30,7 @@ def login_view(request):
                     return redirect('main')
                 else:
                     request.session['email'] = email
-                    send_otp(request)
+                    send_otp(request, email)
                     messages.error(request, "Підтвердіть свою пошту.")
                     return redirect('accounts:code-verification')
         else:
@@ -52,7 +52,7 @@ def register_view(request, *args, **kwargs):
             user = form.save()
             email = form.cleaned_data.get('email')
             request.session['email'] = email
-            send_otp(request)
+            send_otp(request, email)
             return redirect('accounts:code-verification')
     else:
         form = RegisterForm()
@@ -64,22 +64,16 @@ def register_view(request, *args, **kwargs):
 def code_verification_view(request):
     if request.method == 'POST':
         otp_code = ''.join(request.POST.get(f'otp_digit_{i + 1}') for i in range(6))
-        print(otp_code)
         email = request.session.get('email')
-        print(email)
         otp_secret_key = request.session.get('otp_secret_key')
         otp_valid_date = request.session.get('otp_valid_date')
         type = request.session.get('type')
-        print(otp_secret_key, otp_valid_date, type)
 
         if otp_secret_key and otp_valid_date is not None:
             valid_date = datetime.fromisoformat(otp_valid_date)
             if datetime.now() < valid_date:
                 totp = pyotp.TOTP(otp_secret_key, interval=300)
-
-                print(otp_code, totp.verify(otp_code))
                 if totp.verify(otp_code):
-                    print("222")
                     try:
                         user = User.objects.get(email=email)
                         user.is_verified = True
@@ -96,12 +90,10 @@ def code_verification_view(request):
                         messages.error(request, "Користувача з такою поштою не знайдено.")
                         return redirect('accounts:register')
                 else:
-                    print("Ваш код неправильний.")
                     messages.error(request, "Ваш код неправильний.")
                     return redirect('accounts:code-verification')
 
             else:
-                print("Ваш код більше не діє.")
                 messages.error(request, "Ваш код більше не діє.")
                 return redirect('accounts:code-verification')
 
@@ -112,7 +104,7 @@ def resend_otp(request):
     if request.method == 'POST':
         email = request.session.get('email')
         if email:
-            send_otp(request)  # Відправити новий OTP код
+            send_otp(request, email)  # Відправити новий OTP код
             messages.success(request, "Новий код було відправлено.")
         else:
             messages.error(request, "Користувача з такою поштою не знайдено.")
@@ -122,12 +114,11 @@ def resend_otp(request):
 def email_view(request):
     if request.method == 'POST':
         email = request.POST.get('email')
-        print(email)
         try:
             user = User.objects.get(email=email)
             request.session['email'] = email
             request.session['type'] = "new-password"
-            send_otp(request)
+            send_otp(request, email)
             return redirect('accounts:code-verification')
         except User.DoesNotExist:
             messages.error(request, 'Користувача з такою поштою не знайдено.')
@@ -137,7 +128,6 @@ def email_view(request):
 
 def change_password_view(request):
     email = request.session.get('email')
-    print(email)
     try:
         user = User.objects.get(email=email)
         if request.method == 'POST':
