6e499d2f6790fa1d2b6f95baea1e3284c43a20a3
JuliManhupli <juliamangup@gmail.com>
2024-04-08 19:07:06 +0300

    added uploading files to the task

--- ocean_do/ocean_do/aws.py
+++ ocean_do/ocean_do/aws.py
@@ -1,9 +1,10 @@
-import boto3
 import time
+
+import boto3
 from botocore.exceptions import ClientError
 
 from .settings import AWS_STORAGE_BUCKET_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
-
+from tasks.models import File, Task
 
 def upload_avatar_to_s3(user_email, avatar_file):
     s3 = boto3.client('s3',
@@ -30,3 +31,26 @@ def upload_avatar_to_s3(user_email, avatar_file):
     except ClientError as e:
         print(e)
         return None
+
+
+def upload_file_to_s3(file, task_id):
+    s3 = boto3.client('s3',
+                      aws_access_key_id=AWS_ACCESS_KEY_ID,
+                      aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    bucket_name = AWS_STORAGE_BUCKET_NAME
+    task_folder = f'tasks/{task_id}/'
+    file_name = file.name
+    file_path = task_folder + file_name
+
+    s3.upload_fileobj(file, bucket_name, file_path)
+
+    file_instance = File.objects.create(
+        title=file_name,
+        file=file_path
+    )
+
+    # Додати файл до завдання
+    task = Task.objects.get(pk=task_id)
+    task.files.add(file_instance)
+
+    return file_instance
--- ocean_do/tasks/admin.py
+++ ocean_do/tasks/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Task, Tag
+from .models import Task, Tag, File
 
 admin.site.register(Tag)
 admin.site.register(Task)
+admin.site.register(File)
