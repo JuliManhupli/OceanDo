a58a51dadd3b511ce9f29cb2b38089791f07b6fd
JuliManhupli <juliamangup@gmail.com>
2024-04-16 18:43:25 +0300

    added notifications

---
+++ ocean_do/ocean_do/consumers.py
@@ -0,0 +1,32 @@
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+from django.template.loader import get_template
+
+
+class NotificationConsumer(WebsocketConsumer):
+    def connect(self):
+        self.user = self.scope["user"]
+        if not self.user.is_authenticated:
+            self.close()
+            return
+
+        self.GROUP_NAME = "user-notifications"
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.GROUP_NAME, self.channel_name
+        )
+
+        self.accept()
+
+    def disconnect(self, close_code):
+        if self.user.is_authenticated:
+            # Leave room group
+            async_to_sync(self.channel_layer.group_discard)(
+                self.GROUP_NAME, self.channel_name
+            )
+
+    def user_joined(self, event):
+        html = get_template("ocean_do/notification.html").render(
+            context={"username": event["text"]}
+        )
+        self.send(text_data=html)
